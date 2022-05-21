5eb6687a579b5fac6fc8d10f505465ecc6867849
Shrey Suri <shreysuri5@gmail.com>
2022-05-20 22:36:55 -0500

    adding phrases

--- MadLibs_1.py
+++ MadLibs_1.py
@@ -1,2 +1,3 @@
 silly_string_1 = "Hey diddle diddle the %s and the %s the %s %s over the %"
 silly_string_2 = "%s and %s went up a %s to fetch a %s of %s"
+silly_string_3 = "%s had a %s %s whose %s was &s as %s"
