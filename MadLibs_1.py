800f06b92a86fe59306c83d6e0c3341ac0da4c1b
Shrey Suri <shreysuri5@gmail.com>
2022-05-31 20:08:04 -0500

    done with basics

--- MadLibs_1.py
+++ MadLibs_1.py
@@ -5,9 +5,9 @@ silly_string_1 = "Hey diddle diddle the 'noun' and the 'noun' the 'noun' 'past_v
 silly_string_2 = "'proper_noun' and 'proper_noun' went up a 'noun' to fetch a 'collection_noun' of 'plural_noun'"
 silly_string_3 = "'proper_noun' had a 'adjective' 'noun' whose 'noun' was 'adjective' as 'plural_noun'"
 silly_string_4 = "If you're 'adjective' and you know it 'verb' your 'plural_noun'"
-silly_string_5 = "The %s %s %s %s and %s and %s the %s down"
-silly_string_6 = "%s, %s, %s %s have you any %s?"
-silly_string_7 = "My favorite activity is %s with %s"
-silly_string_8 = "I like eating % %s with lots of %s"
+silly_string_5 = "The 'adjective' , 'adjective' 'noun' 'past_verb' and 'past_verb' and 'past_verb' the 'noun' down"
+silly_string_6 = "'onomatopoeia', 'onomatopoeia', 'adjective' 'noun' have you any 'plural_noun'?"
+silly_string_7 = "My favorite activity is 'gerund' with my 'adjective' 'noun'"
+silly_string_8 = "I like eating 'adjective' 'plural_noun' with lots of 'plural_noun'"
 
 
