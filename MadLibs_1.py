d6a8fab7bee149f6317b2f0e1a28650d43d186f6
Shrey Suri <shreysuri5@gmail.com>
2022-05-31 20:02:18 -0500

    working on a guessing system

--- MadLibs_1.py
+++ MadLibs_1.py
@@ -1,8 +1,13 @@
-silly_string_1 = "Hey diddle diddle the %s and the %s the %s %s over the %"
-silly_string_2 = "%s and %s went up a %s to fetch a %s of %s"
-silly_string_3 = "%s had a %s %s whose %s was &s as %s"
-silly_string_4 = "If you're %s and you know it %s your %s"
+import random
+
+
+silly_string_1 = "Hey diddle diddle the 'noun' and the 'noun' the 'noun' 'past_verb' over the 'noun'"
+silly_string_2 = "'proper_noun' and 'proper_noun' went up a 'noun' to fetch a 'collection_noun' of 'plural_noun'"
+silly_string_3 = "'proper_noun' had a 'adjective' 'noun' whose 'noun' was 'adjective' as 'plural_noun'"
+silly_string_4 = "If you're 'adjective' and you know it 'verb' your 'plural_noun'"
 silly_string_5 = "The %s %s %s %s and %s and %s the %s down"
 silly_string_6 = "%s, %s, %s %s have you any %s?"
 silly_string_7 = "My favorite activity is %s with %s"
 silly_string_8 = "I like eating % %s with lots of %s"
+
+
