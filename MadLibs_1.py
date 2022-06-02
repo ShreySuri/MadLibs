3c3e97417c0ad0e105ad69ef94cfb99524782cd1
Shrey Suri <shreysuri5@gmail.com>
2022-06-01 21:28:10 -0500

    almost done

--- MadLibs_1.py
+++ MadLibs_1.py
@@ -39,31 +39,84 @@ while breakdown_list[word_count] != "placeholder":
 breakdown_list.remove("placeholder")
 
 noun = 0
+noun_list = []
 plural_noun = 0
+plural_noun_list = []
 proper_noun = 0
+proper_noun_list = []
 verb = 0
+verb_list = []
 past_verb = 0
+past_verb_list = []
 adjective = 0
+adjective_list = []
 onomatopoeia = 0
+onomatopoeia_list = []
 gerund = 0
+gerund_list = []
 
 
 
 for i in range (0, word_count):
     if breakdown_list[i] == "'noun'":
         noun = noun + 1
+        noun_list.append(i)
     if breakdown_list[i] == "'plural_noun'":
         plural_noun = plural_noun + 1
+        plural_noun_list.append(i)
     if breakdown_list[i] == "'proper_noun'":
         proper_noun = proper_noun + 1
+        proper_noun_list.append(i)
     if breakdown_list[i] == "'verb'":
         verb = verb + 1
+        verb_list.append(i)
     if breakdown_list[i] == "'past_verb'":
         past_verb = past_verb + 1
+        past_verb_list.append(i)
     if breakdown_list[i] == "'adjective'":
         adjective = adjective + 1
+        adjective_list.append(i)
     if breakdown_list[i] == "'onomatopoeia'":
         onomatopoeia = onomatopoeia + 1
+        onomatopoeia_list.append(i)
     if breakdown_list[i] == "'gerund'":
         gerund = gerund + 1
+        gerund_list.append(i)
 
+
+for i in range (0, noun):
+    word = input(print("Please enter a noun. "))
+    word_placement = noun_list[i]
+    breakdown_list[word_placement] = word
+for i in range (0, plural_noun):
+    word = input(print("Please enter a plural noun. "))
+    word_placement = plural_noun_list[i]
+    breakdown_list[word_placement] = word
+for i in range (0, proper_noun):
+    word = input(print("Please enter a proper noun. "))
+    word_placement = proper_noun_list[i]
+    breakdown_list[word_placement] = word
+for i in range (0, verb):
+    word = input(print("Please enter a verb. "))
+    word_placement = verb_list[i]
+    breakdown_list[word_placement] = word
+for i in range (0, past_verb):
+    word = input(print("Please enter a past-tense verb. "))
+    word_placement = past_verb_list[i]
+    breakdown_list[word_placement] = word
+for i in range (0, adjective):
+    word = input(print("Please enter an adjective. "))
+    word_placement = adjective_list[i]
+    breakdown_list[word_placement] = word
+for i in range (0, onomatopoeia):
+    word = input(print("Please enter an onomatopoeia. "))
+    word_placement = onomatopoeia_list[i]
+    breakdown_list[word_placement] = word
+for i in range (0, gerund):
+    word = input(print("Please enter a gerund. "))
+    word_placement = gerund_list[i]
+    breakdown_list[word_placement] = word
+
+
+
+    
