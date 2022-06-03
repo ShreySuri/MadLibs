1dbe73985302cde7bcd739bdb9d6656e8f722526
Shrey Suri <shreysuri5@gmail.com>
2022-06-02 21:17:46 -0500

    fully works now

--- MadLibs_1.py
+++ MadLibs_1.py
@@ -13,7 +13,7 @@ silly_string_2 = "'proper_noun' and 'proper_noun' went up a 'noun' to fetch a 'c
 silly_string_3 = "'proper_noun' had a 'adjective' 'noun' whose 'noun' was 'adjective' as 'plural_noun'"
 silly_string_4 = "If you're 'adjective' and you know it 'verb' your 'plural_noun'"
 silly_string_5 = "The 'adjective' , 'adjective' 'noun' 'past_verb' and 'past_verb' and 'past_verb' the 'noun' down"
-silly_string_6 = "'onomatopoeia', 'onomatopoeia', 'adjective' 'noun' have you any 'plural_noun'?"
+silly_string_6 = "'onomatopoeia' , 'onomatopoeia' , 'adjective' 'noun' have you any 'plural_noun' ?"
 silly_string_7 = "My favorite activity is 'gerund' with my 'adjective' 'noun'"
 silly_string_8 = "I like eating 'adjective' 'plural_noun' with lots of 'plural_noun'"
 
